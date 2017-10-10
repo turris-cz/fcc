c68374fb5fa98dd928c343856aefb674dedac0e5
Michal Hrusecky <michal.hrusecky@nic.cz>
2017-10-10 15:50:08 +0200

    Separate various settings

--- fcc.sh
+++ fcc.sh
@@ -2,59 +2,16 @@
 
 SSH="ssh root@192.168.2.1"
 
-reload_hostapd() {
-    if [ -n "`$SSH cat /var/run/wifi-$PHY.pid`" ]; then
-        $SSH kill `$SSH cat /var/run/wifi-$PHY.pid`
-    fi
-    $SSH /usr/sbin/hostapd -P /var/run/wifi-$PHY.pid -B /var/run/hostapd-$PHY.conf \& > /dev/null 2> /dev/null 
-}
-
-get_channels() {
-    $SSH iw $PHY info | sed -n 's|.*\* \([0-9]*\) MHz \[\([0-9]*\)\] (\([0-9.]*\) dBm.*|channel \2 (\1 MHz) - maximum power \3 dBm|p'
-}
-
-usb_test() {
-    ans=""
-    while [ "$ans" \!= F ] && [ "$ans" \!= B ]; do
-        echo
-        echo "Do you want to read from front usb or back usb? [F/B] "
-        read ans
-    done
-    if [ $ans = B ]; then
-        DEV=$(basename $($SSH ls -1d /sys/bus/usb/devices/5-1/5-1:1.0/host*/target*/*/block/sd[a-z]))
-    else
-        DEV=$(basename $($SSH ls -1d /sys/bus/usb/devices/3-1/3-1:1.0/host*/target*/*/block/sd[a-z]))
-    fi
-    if [ -z "$DEV" ]; then
-        echo "Port is empty"
-    else
-        echo
-        echo "Press Ctrl+C to stop transmitting data"
-        while $SSH cat /dev/$DEV | pv > /dev/null; do
-            true
-        done
-    fi
-}
-
-set_frag() {
-    choose_card
-    echo
-    echo "What should be the new fragmentation threshold (empty = off)?"
-    read ans
-    if [ -n "$ans" ]; then
-        $SSH iw phy $PHY set frag $ans
-    else
-        $SSH iw phy $PHY set frag off
-    fi
-}
-
-set_tx_power() {
-    choose_card
-    echo
-    echo "What should be the new txpower in dBm?"
-    read ans
-    VALUE="`expr $ans \* 100`"
-    $SSH iw phy $PHY set txpower fixed $VALUE
+save_settings() {
+    cat > /tmp/fcc-$WLAN << EOF
+    chnum="$chnum"
+    chw="$chw"
+    SIGN="$SIGN"
+    TXPWR="$TXPWR"
+    BITRATE="$BITRATE"
+    beacon_int="$beacon_int"
+    STATE="$STATE"
+EOF
 }
 
 generate_hostpad() {
@@ -111,35 +68,125 @@ auth_algs=1
 wpa=0
 ssid=Turris-Omnia-Test-`if [ $WLAN = wlan1 ]; then echo ath9k; else echo ath10k; fi`
 bridge=br-lan
-beacon_int=15
+beacon_int=$beacon_int
 bssid=`$SSH ip a s dev wlan0 | sed -n 's|.*link/ether\ \([^[:blank:]]*\)\ .*|\1|p'`
 " | $SSH cat \> /var/run/hostapd-$PHY.conf
+}
 
+reload_hostapd() {
+    generate_hostpad "$chnum" "$chw" "$SIGN"
+    echo "Reloading configuration"
+    if [ -n "`$SSH cat /var/run/wifi-$PHY.pid`" ]; then
+        $SSH kill `$SSH cat /var/run/wifi-$PHY.pid` 2> /dev/null
+        sleep 1
+    fi
+    $SSH /usr/sbin/hostapd -P /var/run/wifi-$PHY.pid -B /var/run/hostapd-$PHY.conf \& > /dev/null 2> /dev/null
+    STATE="ON"
+    sleep 5
+    if [ "$TXPWR" ]; then
+        VALUE="`expr $ans \* 100`"
+        $SSH iw phy $PHY set txpower fixed $VALUE
+    fi
+    if [ "$BITRATE" ]; then
+        $SSH iw dev $WLAN set bitrates $BITRATE
+    fi
+    save_settings
+}
+
+get_channels() {
+    $SSH iw $PHY info | sed -n 's|.*\* \([0-9]*\) MHz \[\([0-9]*\)\] (\([0-9.]*\) dBm.*|channel \2 (\1 MHz) - maximum power \3 dBm|p'
+}
+
+load_card() {
+    PHY=phy$1
+    WLAN=wlan$1
+    chnum=""
+    chw=""
+    SIGN=""
+    TXPWR=""
+    BITRATE=""
+    beacon_int=""
+    STATE="OFF"
+    [ \! -f /tmp/fcc-$WLAN ] || . /tmp/fcc-$WLAN
+    [ -n "$beacon_int" ] || beacon_int=15
+    [ -n "$chnum" ] || chnum=6
+    [ -n "$chw" ] || chw="legacy"
+    [ -n "$SIGN" ] || SIGN="+"
+}
+
+print_settings() {
+    load_card "$1"
+    echo " * current configuration: $STATE - channel: $chnum width: $chw `[ "$chw" = legacy ] || [ "$chw" = 20 ] || echo "HT40$SIGN "``[ -z "$TXPWR" ] || echo "txpower: $TWPWR "``[ -z "$BITRATE" ] || echo "bitrates: $BITRATE "`"
+}
+
+usb_test() {
+    ans=""
+    while [ "$ans" \!= F ] && [ "$ans" \!= B ]; do
+        echo
+        echo "Do you want to read from front usb or back usb? [F/B] "
+        read ans
+    done
+    if [ $ans = B ]; then
+        DEV=$(basename $($SSH ls -1d /sys/bus/usb/devices/5-1/5-1:1.0/host*/target*/*/block/sd[a-z]))
+    else
+        DEV=$(basename $($SSH ls -1d /sys/bus/usb/devices/3-1/3-1:1.0/host*/target*/*/block/sd[a-z]))
+    fi
+    if [ -z "$DEV" ]; then
+        echo "Port is empty"
+    else
+        echo
+        echo "Press Ctrl+C to stop transmitting data"
+        while $SSH cat /dev/$DEV | pv > /dev/null; do
+            true
+        done
+    fi
+}
+
+set_frag() {
+    choose_card
+    echo
+    echo "What should be the new fragmentation threshold (empty = off)?"
+    read ans
+    if [ -n "$ans" ]; then
+        $SSH iw phy $PHY set frag $ans
+    else
+        $SSH iw phy $PHY set frag off
+    fi
+}
+
+set_tx_power() {
+    choose_card
+    echo
+    echo "What should be the new txpower in dBm?"
+    read ans
+    TXPWR=$ans
+    VALUE="`expr $ans \* 100`"
+    $SSH iw phy $PHY set txpower fixed $VALUE
 }
 
 set_channel() {
     choose_card
-    echo
-    echo "WARNING: Changing channel will clear MCS settings"
-    echo
     echo -n "Enter channel number: "
     read chnum
-    echo -n "Enter channel width (legacy, 20, 40 80): "
+    reload_hostapd
+}
+
+set_channel_width() {
+    choose_card
+    echo -n "Enter channel width (legacy, 20, 40`[ $chnum -lt 15 ] || echo ", 80"`): "
     read chw
-    if [ $chw = legacy ]; then
-        WIDTH=""
+    reload_hostapd
+}
+
+set_channel_sign() {
+    choose_card
+    echo -n "Do you want to set HT40+ or HT40-? "
+    read direct
+    if [ $direct = "HT40+" ]; then
+        SIGN="+"
     else
-        if [ $chw -gt 20 ]; then
-            echo -n "HT40+ or HT40-? "
-            read direct
-            if [ $direct = "HT40+" ]; then
-                SIGN="+"
-            else
-                SIGN="-"
-            fi
-        fi
+        SIGN="-"
     fi
-    generate_hostpad "$chnum" "$chw" "$SIGN"
     reload_hostapd
 }
 
@@ -155,6 +202,7 @@ set_beacon() {
     else
         $SSH echo "beacon_int=$num" \>\> /var/run/hostapd-$PHY.conf
     fi
+    beacon_int=$num
     reload_hostapd
 }
 
@@ -166,34 +214,26 @@ choose_card() {
     echo
     echo -n "Your choice? "
     read ans
-    PHY=phy$ans
-    WLAN=wlan$ans
+    load_card $ans
 }
 
 start_default() {
     choose_card
-    if [ $WLAN = wlan0 ]; then
-        generate_hostpad "36" "80" "+"
-    else
-        generate_hostpad "6" "legacy" ""
-    fi
+    rm -f /tmp/fcc-$WLAN
+    load_card $ans
+    $SSH ip l s up dev $WLAN 2> /dev/null
     reload_hostapd
 }
 
 toogle_card() {
     choose_card
     if [ "`$SSH ps w | grep hostapd | grep $PHY`" ]; then
-        $SSH kill \`cat /var/run/wifi-$PHY.pid\`
+        $SSH kill \`cat /var/run/wifi-$PHY.pid\` 2> /dev/null
         $SSH ip l s down dev $WLAN
+        STATE="OFF"
+        save_settings
     else
         $SSH ip l s up dev $WLAN
-        if [ -z "`$SSH ls /var/run/hostapd-$PHY.conf`" ]; then
-            if [ $WLAN = wlan0 ]; then
-                generate_hostpad "36" "80" "+"
-            else
-                generate_hostpad "6" "legacy" ""
-            fi
-        fi
         reload_hostapd
     fi
 }
@@ -228,10 +268,11 @@ set_mcs() {
             3) echo -n "Enter VHT NSS: "; read nss; echo -n "Enter VHT MCS: "; read mcs; VHT="vht-mcs-$BAND $nss:$mcs" ;;
             4) GI="sgi-$BAND" ;;
             5) GI="lgi-$BAND" ;;
-            6) GI=""; LEGACY=""; HT=""; VHT="" ;;
+            6) GI=""; LEGACY=""; HT=""; VHT=""; BITRATE="" ;;
             *) echo "Invalid option!";;
         esac
-        $SSH iw dev $WLAN set bitrates $LEGACY $HT $VHT $GI
+        BITRATE="$LEGACY $HT $VHT $GI"
+        $SSH iw dev $WLAN set bitrates $BITRATE
         sleep 2
     done
 }
@@ -246,7 +287,10 @@ get_wifi_info() {
     else
         echo "$WLAN - off"
     fi
+    print_settings `echo $WLAN | sed 's|wlan||'`
     done
+    echo
+    echo "If your configuration says card should be on and it off, your configuration is probably invalid"
 }
 
 main_menu() {
@@ -266,8 +310,10 @@ main_menu() {
         echo " 3) set tx power"
         echo " 4) set MCS"
         echo " 5) set channel"
-        echo " 6) set beacon interval"
-        echo " 7) set card to default and enable"
+        echo " 6) set channel width"
+        echo " 7) set HT40+/HT40-"
+        echo " 8) set beacon interval"
+        echo " 9) set card to default and enable"
 #        echo " 5) set fragmentation threshold"
         echo
         echo -n "Enter your command: "
@@ -279,8 +325,10 @@ main_menu() {
             3) set_tx_power ;;
             4) set_mcs ;;
             5) set_channel ;;
-            6) set_beacon ;;
-            7) start_default ;;
+            6) set_channel_width ;;
+            7) set_channel_sign ;;
+            8) set_beacon ;;
+            9) start_default ;;
 #            5) set_frag ;;
             *) echo "Invalid option!";;
         esac
@@ -293,6 +341,7 @@ while [ -z "`$SSH echo ok`" ]; do
     sleep 1
 done
 
+rm -f /tmp/fcc-*
 $SSH wifi down
 for i in 0 1; do
     $SSH iw phy$i interface add wlan$i type managed > /dev/null 2>&1
